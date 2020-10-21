4aa28b3315315bcc40b69219c28d71348db45e23
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-10-21 13:56:01 +0200

    Signed-off-by: Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -26,11 +26,43 @@ self.addEventListener("install", installEvent => {
 
 self.addEventListener("fetch", fetchEvent => {
     fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
+      caches.match(fetchEvent.request).then(res => {console.log(res);
         return res || fetch(fetchEvent.request)
       })
     )
   })
-  self.addEventListener('appinstalled', (event) => {
-    console.log('👍', 'appinstalled', event);
-  });  
+
+// self.addEventListener('fetch', event => {
+//   const {request} = event;
+//   const url = new URL(request.url);
+//   if(url.origin === location.origin) {
+//       event.respondWith(cacheData(request));
+//   } else {
+//       event.respondWith(networkFirst(request));
+//   }
+
+// });
+
+
+// async function cacheData(request) {
+//   const cachedResponse = await caches.match(request);
+//   return cachedResponse || fetch(request);
+// }
+
+// async function networkFirst(request) {
+//   const cache = await caches.open('dynamic-meme');
+
+//   try {
+//       const response = await fetch(request);
+//       cache.put(request, response.clone());
+//       return response;
+//   } catch (error){
+//       return await cache.match(request);
+
+//   }
+
+// }
+
+//   self.addEventListener('appinstalled', (event) => {
+//     console.log('👍', 'appinstalled', event);
+//   });  
